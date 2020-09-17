f104a59ad011d09f5225a6791ef0d2bd3ee38ff2
Marco Bagatelli <manhani@gmail.com>
2020-09-17 15:27:35 +0100

    Added Add-Ins & Polished script

--- Script 1-2.ps1
+++ Script 1-2.ps1
@@ -1,11 +1,18 @@
-﻿Import-Module 'C:\Program Files\Microsoft Dynamics 365 Business Central\140\Service\NavAdminTool.ps1'
+﻿# Before running delete all old tables and UPG tables
+
+###################### Parameters ######################
+$ServiceName          = "BC140"
+##################################################################
+
+
+Import-Module 'C:\Program Files\Microsoft Dynamics 365 Business Central\140\Service\NavAdminTool.ps1'
 Import-Module 'C:\Program Files (x86)\Microsoft Dynamics 365 Business Central\140\RoleTailored Client\NavModelTools.ps1'
 Import-Module 'C:\Program Files (x86)\Microsoft Dynamics 365 Business Central\140\RoleTailored Client\Microsoft.Dynamics.Nav.Ide.psm1'
 
-Get-NAVAppInfo -ServerInstance bc140 | % { Uninstall-NAVApp -ServerInstance bc140 -Name $_.Name -Version $_.Version -Force}
+Get-NAVAppInfo -ServerInstance $ServiceName | % { Uninstall-NAVApp -ServerInstance $ServiceName -Name $_.Name -Version $_.Version -Force}
 
-Get-NAVAppInfo -ServerInstance bc140 | % { Unpublish-NAVApp -ServerInstance bc140 -Name $_.Name -Version $_.Version }
+Get-NAVAppInfo -ServerInstance $ServiceName | % { Unpublish-NAVApp -ServerInstance $ServiceName -Name $_.Name -Version $_.Version }
 
-Get-NAVAppInfo -ServerInstance bc140 -SymbolsOnly | % { Unpublish-NAVApp -ServerInstance bc140 -Name $_.Name -Version $_.Version }
+Get-NAVAppInfo -ServerInstance $ServiceName -SymbolsOnly | % { Unpublish-NAVApp -ServerInstance $ServiceName -Name $_.Name -Version $_.Version }
 
-Stop-NAVServerInstance -ServerInstance bc140
+Stop-NAVServerInstance -ServerInstance $ServiceName
--- script 2-2.ps1
+++ script 2-2.ps1
@@ -1,7 +1,7 @@
 ﻿###################### Parameters ######################
 $DatabaseServer       = "ARQ-MBG"
 #$DatabaseInstance     = "BC160"
-$DatabaseName         = "GTOGHERMIGRATEDB_14to16_WORKV3" 
+$DatabaseName         = "GTOGHERMIGRATEDB_14to16_WORKV5" 
 $ServiceName          = "BC160"
 $DeveloperLicenseFile = "C:\Temp\MSDynLicenses\5190281 - D365BC 160 DEV.flf"
 $BC15Version          = "15.8.43801.0"
@@ -11,81 +11,131 @@ $MicrosoftSysPath     = "C:\Temp\Dynamics.365.BC.15953.W1.DVD_BC16\applications\
 $MicrosoftApplicPath  = "C:\Temp\Dynamics.365.BC.15953.W1.DVD_BC16\applications\Application\Source\Microsoft_Application.app"
 $CustomAppPath        = "C:\Users\Marco Bagatelli\Documents\AL\BC14toBC15\Growing Together_BC14toBC15_$CustomAppVersion.app"
 $CustomAppName        = "BC14toBC15"
-$CustomAppVersion     = "3.0.0.13"
-$ServerInstance       = "BC160"
+#$CustomAppVersion     = "3.0.0.13"
+$ServicesAddinsFolder = 'C:\Program Files\Microsoft Dynamics 365 Business Central\160\Service\Add-ins'
 
 ##################################################################
 
-
 Import-Module 'C:\Program Files\Microsoft Dynamics 365 Business Central\160\Service\NavAdminTool.ps1'
 
-Write-Host "Run the Invoke-NAVApplicationDatabaseConversion cmdlet to start the conversion"
-Invoke-NAVApplicationDatabaseConversion -DatabaseServer $DatabaseServer -DatabaseName $DatabaseName
+Write-Host "1. Run the Invoke-NAVApplicationDatabaseConversion cmdlet to start the conversion"
+Invoke-NAVApplicationDatabaseConversion -DatabaseServer $DatabaseServer -DatabaseName $DatabaseName -Force
 
-Write-Host "Set the server instance to connect to the application database."
+Write-Host "2. Set the server instance to connect to the application database."
 Set-NAVServerConfiguration -ServerInstance $ServiceName -KeyName DatabaseName -KeyValue $DatabaseName
 
-Write-Host "Configure the DestinationAppsForMigration setting of the server instance to table migration extension."
+Write-Host "3. Configure the DestinationAppsForMigration setting of the server instance to table migration extension."
 Set-NAVServerConfiguration -ServerInstance $ServiceName -KeyName "DestinationAppsForMigration" -KeyValue '[{"appId":"335d5fa2-777c-4340-a45e-8b92cf21860e", "name":"MicrosoftBC14to16", "publisher": "Marco Bagatelli"}]'
 
-Write-Host "Disable task scheduler on the server instance for purposes of upgrade."
+Write-Host "4. Disable task scheduler on the server instance for purposes of upgrade."
 Set-NavServerConfiguration -ServerInstance $ServiceName -KeyName "EnableTaskScheduler" -KeyValue false
 
-Write-Host "Restart the server instance."
+Write-Host "5. Restart the server instance."
 Restart-NAVServerInstance -ServerInstance $ServiceName
 
-Write-Host "Import the version 16 partner license."
+Write-Host "6. Import the version 16 partner license."
 Import-NAVServerLicense -ServerInstance $ServiceName -LicenseFile $DeveloperLicenseFile
 
-Write-Host "Restart the server instance."
+Write-Host "7. Restart the server instance."
 Restart-NAVServerInstance -ServerInstance $ServiceName
 
-Write-Host "Change the application version"
+Write-Host "8. Change the application version"
 Set-NAVApplication -ServerInstance $ServiceName -ApplicationVersion 16.5.15941 -Force
 
-Write-Host "Publish version 16 system symbols extension."
+Write-Host "9. Publish version 16 system symbols extension."
 Publish-NAVApp -ServerInstance $ServiceName -Path $SystemAppPath -PackageType SymbolsOnly
 
-Write-Host "Publish the first version of the table migration extension, which is the version that contains the table objects."
+Write-Host "10. Publish the first version of the table migration extension, which is the version that contains the table objects."
 Publish-NAVApp -ServerInstance $ServiceName -Path "C:\Users\Marco Bagatelli\Documents\AL\Vamos\MicrosoftBC14to16\Marco Bagatelli_MicrosoftBC14toBC16_1.0.0.0.app" -SkipVerification
 
-Write-Host "Publish the empty versions of the following extensions"
+Write-Host "11. Publish the empty versions of the following extensions"
 Publish-NAVApp -ServerInstance $ServiceName -Path "C:\Users\Marco Bagatelli\Documents\AL\Vamos\SysApplication\Microsoft_System Application_14.0.0.0.app" -SkipVerification
 Publish-NAVApp -ServerInstance $ServiceName -Path "C:\Users\Marco Bagatelli\Documents\AL\Vamos\BaseApp\Microsoft_Base Application_14.0.0.0.app" -SkipVerification
 Publish-NAVApp -ServerInstance $ServiceName -Path "C:\Users\Marco Bagatelli\Documents\AL\Vamos\BC14toBC15\Growing Together_BC14toBC15_1.0.0.0.app" -SkipVerification
 
-Write-Host "Restart the Service"
+Write-Host "12. Restart the Service"
 Restart-NAVServerInstance -ServerInstance $ServiceName
 
-Write-Host "Synchronize the tenant with the application database."
-Sync-NAVTenant -ServerInstance $ServiceName -Mode Sync
+Write-Host "13. Synchronize the tenant with the application database."
+Sync-NAVTenant -ServerInstance $ServiceName -Mode Sync -Force
 
-Write-Host "Synchronize the tenant with the table migration extension."
+#Write-Host "14. Pausing for 1 minute"
+#Start-Sleep -Seconds 60
+
+Write-Host "15. Synchronize the tenant with the table migration extension."
 Sync-NAVApp -ServerInstance $ServiceName -Name "MicrosoftBC14toBC16" -Version 1.0.0.0
 Sync-NAVApp -ServerInstance $ServiceName -Name "System Application" -Version 14.0.0.0
 Sync-NAVApp -ServerInstance $ServiceName -Name "Base Application" -Version 14.0.0.0
 Sync-NAVApp -ServerInstance $ServiceName -Name "BC14toBC15" -Version 1.0.0.0
 
-Write-Host "Run the Data Upgrade"
-Start-NAVDataUpgrade -ServerInstance $ServiceName -FunctionExecutionMode Serial -SkipCompanyInitialization #-SkipAppVersionCheck
+Write-Host "16. Run the Data Upgrade"
+Start-NAVDataUpgrade -ServerInstance $ServiceName -FunctionExecutionMode Serial -SkipCompanyInitialization -Force #-SkipAppVersionCheck 
 
 ## It will stop here, need to wait 3-5m for the data process to finish
-#Write-Host "Pausing for 4 minutes"
-#Start-Sleep -Seconds 240
+Write-Host "17. Pausing for 4 minutes"
+Start-Sleep -Seconds 240
 
-Write-Host "Install the empty versions of the system, base, and custom extensions that you published."
+Write-Host "18. Install the empty versions of the system, base, and custom extensions that you published."
 Install-NAVApp -ServerInstance $ServiceName -Name "System Application" -Version 14.0.0.0
 Install-NAVApp -ServerInstance $ServiceName -Name "Base Application" -Version 14.0.0.0
 Install-NAVApp -ServerInstance $ServiceName -Name "BC14toBC15" -Version 1.0.0.0
 
-Write-Host "Publish extensions using the Publish-NAVApp cmdlet like you did in previous steps."
+Write-Host "19. Publish the extensions."
 Publish-NAVApp -ServerInstance $ServiceName -Path "C:\Users\Marco Bagatelli\Documents\AL\Vamos\MicrosoftBC14to16\Marco Bagatelli_MicrosoftBC14toBC16_1.0.0.1.app" -SkipVerification
 Publish-NAVApp -ServerInstance $ServiceName -Path $MicrosoftSysPath
 Publish-NAVApp -ServerInstance $ServiceName -Path $BaseAppPath
 Publish-NAVApp -ServerInstance $ServiceName -Path "C:\Users\Marco Bagatelli\Documents\AL\Vamos\BC14toBC15\Growing Together_BC14toBC15_1.0.0.1.app" -SkipVerification
 
-Write-Host "Synchronize the newly published extensions using the Sync-NAVApp cmdlet like you did in previous steps."
+Write-Host "20. Synchronize the newly published extensions using the Sync-NAVApp cmdlet like you did in previous steps."
 Sync-NAVApp -ServerInstance $ServiceName -Name "System Application" -Version 16.5.15897.15953
 Sync-NAVApp -ServerInstance $ServiceName -Name "Base Application" -Version 16.5.15897.15953
 Sync-NAVApp -ServerInstance $ServiceName -Name "BC14toBC15" -Version 1.0.0.1
 Sync-NAVApp -ServerInstance $ServiceName -Name "MicrosoftBC14toBC16" -Version 1.0.0.1
+
+Write-Host "21. Run data upgrade on the table migration extension"
+Start-NAVAppDataUpgrade -ServerInstance $ServiceName -Name "MicrosoftBC14toBC16" -version 1.0.0.1
+
+Write-Host "22. Upgrade final extensions"
+Start-NAVAppDataUpgrade -ServerInstance $ServiceName -Name "System Application" -version 16.5.15897.15953
+Start-NAVAppDataUpgrade -ServerInstance $ServiceName -Name "Base Application" -Version 16.5.15897.15953
+Start-NAVAppDataUpgrade -ServerInstance $ServiceName -Name "BC14toBC15" -Version 1.0.0.1
+
+Write-Host "23. Upgrade control add-ins"
+Set-NAVAddIn -ServerInstance $ServiceName -AddinName 'Microsoft.Dynamics.Nav.Client.BusinessChart' -PublicKeyToken 31bf3856ad364e35 -ResourceFile ($AppName = Join-Path $ServicesAddinsFolder 'BusinessChart\Microsoft.Dynamics.Nav.Client.BusinessChart.zip')
+Set-NAVAddIn -ServerInstance $ServiceName -AddinName 'Microsoft.Dynamics.Nav.Client.FlowIntegration' -PublicKeyToken 31bf3856ad364e35 -ResourceFile ($AppName = Join-Path $ServicesAddinsFolder 'FlowIntegration\Microsoft.Dynamics.Nav.Client.FlowIntegration.zip')
+Set-NAVAddIn -ServerInstance $ServiceName -AddinName 'Microsoft.Dynamics.Nav.Client.OAuthIntegration' -PublicKeyToken 31bf3856ad364e35 -ResourceFile ($AppName = Join-Path $ServicesAddinsFolder 'OAuthIntegration\Microsoft.Dynamics.Nav.Client.OAuthIntegration.zip')
+Set-NAVAddIn -ServerInstance $ServiceName -AddinName 'Microsoft.Dynamics.Nav.Client.PageReady' -PublicKeyToken 31bf3856ad364e35 -ResourceFile ($AppName = Join-Path $ServicesAddinsFolder 'PageReady\Microsoft.Dynamics.Nav.Client.PageReady.zip')
+Set-NAVAddIn -ServerInstance $ServiceName -AddinName 'Microsoft.Dynamics.Nav.Client.PowerBIManagement' -PublicKeyToken 31bf3856ad364e35 -ResourceFile ($AppName = Join-Path $ServicesAddinsFolder 'PowerBIManagement\Microsoft.Dynamics.Nav.Client.PowerBIManagement.zip')
+Set-NAVAddIn -ServerInstance $ServiceName -AddinName 'Microsoft.Dynamics.Nav.Client.RoleCenterSelector' -PublicKeyToken 31bf3856ad364e35 -ResourceFile ($AppName = Join-Path $ServicesAddinsFolder 'RoleCenterSelector\Microsoft.Dynamics.Nav.Client.RoleCenterSelector.zip')
+Set-NAVAddIn -ServerInstance $ServiceName -AddinName 'Microsoft.Dynamics.Nav.Client.SatisfactionSurvey' -PublicKeyToken 31bf3856ad364e35 -ResourceFile ($AppName = Join-Path $ServicesAddinsFolder 'SatisfactionSurvey\Microsoft.Dynamics.Nav.Client.SatisfactionSurvey.zip')
+Set-NAVAddIn -ServerInstance $ServiceName -AddinName 'Microsoft.Dynamics.Nav.Client.SocialListening' -PublicKeyToken 31bf3856ad364e35 -ResourceFile ($AppName = Join-Path $ServicesAddinsFolder 'SocialListening\Microsoft.Dynamics.Nav.Client.SocialListening.zip')
+Set-NAVAddIn -ServerInstance $ServiceName -AddinName 'Microsoft.Dynamics.Nav.Client.VideoPlayer' -PublicKeyToken 31bf3856ad364e35 -ResourceFile ($AppName = Join-Path $ServicesAddinsFolder 'VideoPlayer\Microsoft.Dynamics.Nav.Client.VideoPlayer.zip')
+Set-NAVAddIn -ServerInstance $ServiceName -AddinName 'Microsoft.Dynamics.Nav.Client.WebPageViewer' -PublicKeyToken 31bf3856ad364e35 -ResourceFile ($AppName = Join-Path $ServicesAddinsFolder 'WebPageViewer\Microsoft.Dynamics.Nav.Client.WebPageViewer.zip')
+Set-NAVAddIn -ServerInstance $ServiceName -AddinName 'Microsoft.Dynamics.Nav.Client.WelcomeWizard' -PublicKeyToken 31bf3856ad364e35 -ResourceFile ($AppName = Join-Path $ServicesAddinsFolder 'WelcomeWizard\Microsoft.Dynamics.Nav.Client.WelcomeWizard.zip')
+
+Write-Host "24. Enable task scheduler on the server instance."
+Set-NavServerConfiguration -ServerInstance $ServiceName -KeyName "EnableTaskScheduler" -KeyValue true
+
+Write-Host "25. Restart the Service"
+Restart-NAVServerInstance -ServerInstance $ServiceName
+
+Write-Host "26 Unpublishing apps"
+Unpublish-NAVApp -ServerInstance $ServiceName -Name "Base Application" -Version 14.0.0.0
+Unpublish-NAVApp -ServerInstance $ServiceName -Name "System Application" -Version 14.0.0.0
+Unpublish-NAVApp -ServerInstance $ServiceName -Name "BC14toBC15" -Version 1.0.0.0
+Unpublish-NAVApp -ServerInstance $ServiceName -Name "MicrosoftBC14toBC16" -Version 1.0.0.0
+
+Write-Host "27 Publish & Install System App"
+Publish-NAVApp -ServerInstance $ServiceName -Path $MicrosoftApplicPath -SkipVerification
+Sync-NAVApp -ServerInstance $ServiceName -Name "Application" -Version 16.5.15897.15953 
+Install-NAVApp -ServerInstance $ServerInstance -Name "Application" -Version 16.5.15897.15953
+
+
+
+#    If you want to use data encryption as before, enable it.
+#
+#    Grant users permission to the Open in Excel and Edit in Excel actions.
+#    Version 16 introduces a system permission that protects these two actions. The permission is granted by the system object 6110 Allow Action Export To Excel. 
+#    Because of this change, users who had permission to these actions before upgrading, will lose permission. To grant permission again, do one of the following steps:
+#    Assign the EXCEL EXPORT ACTION permission set to appropriate users.
+#    Add the system object 6110 Allow Action Export To Excel permission directly to appropriate permission sets.
